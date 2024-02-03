f0d336661c12232478be2336ea182dd498f29172
Julian Hammer <jh@moinjulian.com>
2024-02-03 13:29:31 +0000

    Sass styles

--- client/src/lib/Header.svelte
+++ client/src/lib/Header.svelte
@@ -2,3 +2,21 @@
     <h1>Chat App</h1>
     <p>Made with Svelte and socket.io</p>
 </header>
+
+<style lang="scss">
+    header {
+        background-color: var(--bg-color-2);
+        padding-block: 1.5rem 1rem;
+        text-align: center;
+    }
+
+    h1 {
+        font-weight: 400;
+    }
+
+    p {
+        font-size: smaller;
+        margin-top: 0.5rem;
+        color: var(--dark-font-color);
+    }
+</style>
