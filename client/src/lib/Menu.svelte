3b1a7096a3963adc77b0a8f7718520507f97380d
Julian Hammer <jh@moinjulian.com>
2024-02-03 21:09:00 +0000

    Menu Styles

--- client/src/lib/Menu.svelte
+++ client/src/lib/Menu.svelte
@@ -13,3 +13,14 @@
     </button>
     <button on:click={reload_page}>Logout</button>
 </menu>
+
+<style>
+    menu {
+        padding: 0.75rem;
+        display: flex;
+        gap: 0.5rem;
+        align-items: flex-start;
+        justify-content: flex-end;
+        border-bottom: 0.1rem solid var(--seperator-color);
+    }
+</style>
