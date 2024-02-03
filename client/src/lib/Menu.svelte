7db815fea122009d1b1e7959fbf66bae159b7be0
Julian Hammer <jh@moinjulian.com>
2024-02-03 10:59:48 +0000

    Logout

--- client/src/lib/Menu.svelte
+++ client/src/lib/Menu.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
+  import { reload_page } from "@/utils";
+
     export let show_users = false;
 </script>
 
-<button on:click={() => show_users = !show_users}>
-    {#if show_users}
-        Show Chat
-    {:else}
-        Show Users
-    {/if}
-</button>
+<menu>
+    <button on:click={() => show_users = !show_users}>
+        {#if show_users}
+            Show Chat
+        {:else}
+            Show Users
+        {/if}
+    </button>
+    <button on:click={reload_page}>Logout</button>
+</menu>
