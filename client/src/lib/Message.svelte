16261edd6564c3389b99ed75e48de0aa2960dff4
Julian Hammer <jh@moinjulian.com>
2024-02-04 11:40:21 +0000

    Message styles

--- client/src/lib/Message.svelte
+++ client/src/lib/Message.svelte
@@ -4,12 +4,18 @@
 
 <li class:bot={message.bot}>
     {#if "user_name" in message}
-         <span>{message.user_name}:</span>
+         <span class="user">{message.user_name}:</span>
     {/if}
     <span>{message.text}</span>
 </li>
 
 <style>
+    li {
+        margin-bottom: 0.5rem;
+    }
+    .user {
+        color: var(--dark-font-color);
+    }
     .bot {
         font-weight: bold;
     }
